0258cf87d82a30d8435459c1bb1249ec7e77ca14
Alistair <alistairgarioch@gmail.com>
2022-02-12 15:57:03 +0000

    feat(server): add cors middleware on server

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 8080;
+
+//! NOTE: Normally I would move config to a .env file,
+const PORT = 8080;
+const CLIENT = 'http://localhost:3000';
 
 const data = require('./data');
 
-app.get("/data", (req, res) => {
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', CLIENT);
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+//! NOTE: normally I would follow a MVC pattern for the server with routes, & controllers in separate folders,
+// Routes
+app.get('/data', (req, res) => {
   res.json(data);
 });
 
-app.listen(port, () => {
-  console.log(`API listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`API listening at http://localhost:${PORT}`);
 });
